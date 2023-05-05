8a9dc344c84542f10d578bd26162b1d544ddb49a
UserGitHub37 <bum37@mail.ru>
2023-05-06 00:56:55 +0300

    feat: create the Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Card, { CardVariant } from './components/Card';
 
 const App = () => {
   return (
     <div>
-      Все работает!!!
+      <Card onClick={(num) => console.log('click-1', num)} variant={CardVariant.outlined} width='200px' height='200px'>
+        <button style={{background: 'lightblue'}}>Кнопка</button>
+        <div>Lorem ipsum dolor sit amet.</div>
+      </Card>
+      <Card onClick={(num) => console.log('click-2', num)} variant={CardVariant.primary} width='200px' height='100px'>
+        <div>Lorem ipsum dolor sit amet.</div>
+      </Card>
     </div>
   );
 };
